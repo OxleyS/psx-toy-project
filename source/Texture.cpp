fda88efab8522b44397829c00f837377964b9506
Sean Oxley <oxleys314@gmail.com>
2016-12-10 16:41:58 +0900

    Converted the checkerboard texture to use a CLUT.

--- source/Texture.cpp
+++ source/Texture.cpp
@@ -13,3 +13,33 @@ void Texture::InitFromMemoryColor(SemitransparentCode semiTransCode, u_long* pTe
     this->m_TpageId = LoadTPage(pTexels, 2, (int)semiTransCode,
         vramX, vramY, texelWidth, texelHeight);
 }
+
+void Texture::InitFromMemoryClut(SemitransparentCode semiTransCode, u_long* pTexels,
+    int texelWidth, int texelHeight, int vramX, int vramY,
+    Type clutType, u_long* pClut, int clutX, int clutY)
+{
+    assert(clutType == TEXTYPE_CLUT4 || clutType == TEXTYPE_CLUT8);
+
+    int tpRatio, clutCode;
+    if (clutType == TEXTYPE_CLUT4)
+    {
+        tpRatio = 8;
+        clutCode = 0;
+        this->m_ClutId = LoadClut2(pClut, clutX, clutY);
+    }
+    else
+    {
+        tpRatio = 4;
+        clutCode = 1;
+        this->m_ClutId = LoadClut(pClut, clutX, clutY);
+    }
+
+    this->m_Type = clutType;
+    this->m_SemiTransCode = semiTransCode;
+    this->m_TexelWidth = texelWidth;
+    this->m_TexelHeight = texelHeight;
+    this->m_PixelWidth = texelWidth / tpRatio;
+    this->m_PixelHeight = texelHeight / tpRatio;
+    this->m_TpageId = LoadTPage(pTexels, clutCode, (int)semiTransCode,
+        vramX, vramY, texelWidth, texelHeight);
+}
